9cee4dc076fbe5d8256a48d91c5c779938847a76
Michael Hoffmann <michi@hey.com>
2023-04-09 21:33:21 +0200

    Add Sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -42,7 +43,7 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            Integration::captureUnhandledException($e);
         });
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import './bootstrap'
+
+import * as Sentry from "@sentry/browser"
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+    release: import.meta.env.VITE_SENTRY_RELEASE,
+    integrations: [
+        new Sentry.BrowserTracing()
+    ],
+    tracesSampleRate: import.meta.env.VITE_SENTRY_TRACES_SAMPLE_RATE,
+})
